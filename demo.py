ccc543d403421d56f05aa9200c38faca243dae1d
Adrian Tang <ajt6426@psu.edu>
2026-03-26 14:14:44 -0400

    Initial Commit

--- demo.py
+++ demo.py
@@ -1,2 +1,4 @@
 if __name__ == "__main__":
     print("Hello, World!")
+
+# demo.py is a simple Python script that prints "Hello, World!" to the console when executed. It serves as a basic example of a Python program and can be used as a starting point for learning Python programming.
